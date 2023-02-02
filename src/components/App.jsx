8a854a763498c425a13c249923ffae8bf8423c51
win0rdie <rostyslavhnatovskyi@gmail.com>
2023-02-02 17:35:57 +0200

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import { Component } from 'react';
+import { useState } from 'react';
+
 import {
   AddItemForm,
   Button,
@@ -11,206 +12,397 @@ import {
   TutorsList,
   UniversityCard,
 } from '../components';
+import FORMS from 'constants/form';
+
 import universityData from '../constants/universityData';
+
 import TutorIcon from '../assets/images/teachers-emoji.png';
 import CityIcon from '../assets/images/cities.svg';
 import DepartmentIcon from '../assets/images/faculties-emoji.png';
 import AddIcon from '../assets/images/add.svg';
-import FORMS from 'constants/form';
 
-export class App extends Component {
-  state = {
-    tutors: universityData?.tutors ?? [],
-    cities:
-      universityData?.cities.map(city => ({
-        text: city,
-        relation: 'cities',
-      })) ?? [],
-    departments:
-      universityData?.department.map(({ name }) => ({
-        text: name,
-        relation: 'departments',
-      })) ?? [],
-    showForm: null,
+export default function App() {
+  const [tutors, setTutors] = useState(universityData?.tutors ?? []);
+  const [cities, setCities] = useState(
+    universityData?.cities.map(city => ({
+      text: city,
+      relation: 'cities',
+    })) ?? []
+  );
+  const [departments, setDepartments] = useState(
+    universityData?.department.map(({ name }) => ({
+      text: name,
+      relation: 'departments',
+    })) ?? []
+  );
+
+  const [showForm, setShowForm] = useState(null);
+
+  const addTutor = tutor => {
+    setTutors([...tutors, tutor]);
+    setShowForm(null);
   };
 
-  addTutor = tutor => {
-    this.setState(({ tutors }) => ({
-      tutors: [...tutors, tutor],
-      showForm: null,
-    }));
+  const deleteTutor = emailInput => {
+    setTutors([...tutors].filter(({ email }) => email !== emailInput));
   };
 
-  deleteTutor = emailInput => {
-    this.setState(({ tutors }) => {
-      console.log([...tutors]);
-      return {
-        tutors: [...tutors].filter(({ email }) => email !== emailInput),
-      };
-    });
-  };
-
-  onEdit = () => {
+  const onEdit = () => {
     console.log('edit');
   };
 
-  onDelete = () => {
+  const onDelete = () => {
     console.log('delete');
   };
 
-  handleShowForm = formName => {
-    this.setState(prevState => ({
-      showForm: prevState.showForm === formName ? null : formName,
-    }));
+  const handleShowForm = formName => {
+    setShowForm(showForm === formName ? null : formName);
   };
 
-  handleToogleMenu = () => {
-    console.log('card');
-  };
+  // const handleToogleMenu = () => {
+  //   console.log('card');
+  // };
 
-  addCity = name => {
-    if (
-      this.state.cities.some(
-        city => city.text.toLowerCase() === name.toLowerCase()
-      )
-    ) {
+  const addCity = name => {
+    if (cities.some(city => city.text.toLowerCase() === name.toLowerCase())) {
       alert('City already exists');
     } else {
       const newCity = {
         text: name,
         relation: 'cities',
       };
-      this.setState(prevState => ({
-        cities: [...prevState.cities, newCity],
-        showForm: null,
-      }));
+      setCities([...cities, newCity]);
+      setShowForm(null);
     }
   };
 
-  addDepartment = name => {
+  const addDepartment = name => {
     if (
-      this.state.departments.some(
+      departments.some(
         department => department.text.toLowerCase() === name.toLowerCase()
       )
     ) {
       alert('Department already exists');
-    }
-    // else if (
-    //   this.state.departments.some(department => department.text === '')
-    // ) {
-    //   alert('Input is empty');
-    // }
-    else {
+    } else {
       const newDepartment = {
         text: name,
         relation: 'departments',
       };
-      this.setState(prevState => ({
-        departments: [...prevState.departments, newDepartment],
-        showForm: null,
-      }));
+      setDepartments([...departments, newDepartment]);
+      setShowForm(null);
     }
   };
 
-  handleDeleteCard = (id, relation) => {
-    this.setState(prevState => ({
-      [relation]: prevState[relation].filter(({ text }) => text !== id),
-    }));
+  const handleDeleteCard = (id, relation) => {
+    if (relation === 'cities') {
+      const newCitiesArray = cities.filter(({ text }) => text !== id);
+      setCities(newCitiesArray);
+    } else {
+      const newDepartmentsArray = departments.filter(({ text }) => text !== id);
+      setDepartments(newDepartmentsArray);
+    }
   };
 
-  handleEditCard = data => {
+  const handleEditCard = data => {
     const { id, relation, name } = data;
-    const elemIndex = this.state[relation].findIndex(item => item.text === id);
-    this.setState(prevState => ({
-      [relation]: [
-        ...prevState[relation].slice(0, elemIndex),
+
+    if (relation === 'cities') {
+      const indexCity = cities.findIndex(item => item.text === id);
+      setCities(prev => [
+        ...prev.slice(0, indexCity),
         { text: name, relation },
-        ...prevState[relation].slice(elemIndex + 1),
-      ],
-    }));
+        ...prev.slice(indexCity + 1),
+      ]);
+    } else {
+      const indexDepartmnets = departments.findIndex(item => item.text === id);
+      setDepartments(prev => [
+        ...prev.slice(0, indexDepartmnets),
+        { text: name, relation },
+        ...prev.slice(indexDepartmnets + 1),
+      ]);
+    }
   };
 
-  render() {
-    const { tutors, cities, departments, showForm } = this.state;
-    console.log(cities);
+  return (
+    <div className="app">
+      <Sidebar />
 
-    return (
-      <div className="app">
-        <Sidebar />
+      <Main>
+        <Section title="Information about university" isRightPosition isColumn>
+          <UniversityCard
+            name={universityData.name}
+            onEdit={onEdit}
+            onDelete={onDelete}
+          ></UniversityCard>
+          <Paper>
+            <span>{universityData.description}</span>
+          </Paper>
+        </Section>
 
-        <Main>
-          <Section
-            title="Information about university"
-            isRightPosition
-            isColumn
-          >
-            <UniversityCard
-              name={universityData.name}
-              onEdit={this.onEdit}
-              onDelete={this.onDelete}
-            ></UniversityCard>
-            <Paper>
-              <span>{universityData.description}</span>
-            </Paper>
-          </Section>
+        <Section title="Tutors" image={TutorIcon}>
+          <TutorsList tutors={tutors} deleteTutor={deleteTutor} />
+          {showForm === FORMS.TUTOR_FORM && <TutorForm addTutor={addTutor} />}
+          <Button
+            text={showForm === FORMS.TUTOR_FORM ? 'Close form' : 'Add tutor'}
+            image={AddIcon}
+            action={() => handleShowForm(FORMS.TUTOR_FORM)}
+          />
+        </Section>
 
-          <Section title="Tutors" image={TutorIcon}>
-            <TutorsList tutors={tutors} deleteTutor={this.deleteTutor} />
-            {showForm === FORMS.TUTOR_FORM && (
-              <TutorForm addTutor={this.addTutor} />
-            )}
-            <Button
-              text={showForm === FORMS.TUTOR_FORM ? 'Close form' : 'Add tutor'}
-              image={AddIcon}
-              action={() => this.handleShowForm(FORMS.TUTOR_FORM)}
+        <Section title="Cities" image={CityIcon}>
+          <GeneralCardList
+            listData={cities}
+            deleteCard={handleDeleteCard}
+            editCard={handleEditCard}
+          />
+          {showForm === FORMS.CITY_FORM && (
+            <AddItemForm
+              title="add city"
+              placeholder="City"
+              onSubmit={addCity}
             />
-          </Section>
+          )}
+          <Button
+            text={showForm === FORMS.CITY_FORM ? 'Close form' : 'Add city'}
+            image={AddIcon}
+            action={() => handleShowForm(FORMS.CITY_FORM)}
+          />
+        </Section>
 
-          <Section title="Cities" image={CityIcon}>
-            <GeneralCardList
-              listData={cities}
-              deleteCard={this.handleDeleteCard}
-              editCard={this.handleEditCard}
+        <Section title="Departments" image={DepartmentIcon}>
+          <GeneralCardList
+            listData={departments}
+            deleteCard={handleDeleteCard}
+            editCard={handleEditCard}
+          />
+          {showForm === FORMS.DEPARTMENT_FORM && (
+            <AddItemForm
+              title="add departments"
+              placeholder="Departments"
+              onSubmit={addDepartment}
             />
-            {showForm === FORMS.CITY_FORM && (
-              <AddItemForm
-                title="add city"
-                placeholder="City"
-                onSubmit={this.addCity}
-              />
-            )}
-            <Button
-              text={showForm === FORMS.CITY_FORM ? 'Close form' : 'Add city'}
-              image={AddIcon}
-              action={() => this.handleShowForm(FORMS.CITY_FORM)}
-            />
-          </Section>
-
-          <Section title="Departments" image={DepartmentIcon}>
-            <GeneralCardList
-              listData={departments}
-              deleteCard={this.handleDeleteCard}
-              editCard={this.handleEditCard}
-            />
-            {showForm === FORMS.DEPARTMENT_FORM && (
-              <AddItemForm
-                title="add departments"
-                placeholder="Departments"
-                onSubmit={this.addDepartment}
-              />
-            )}
-            <Button
-              text={
-                showForm === FORMS.DEPARTMENT_FORM
-                  ? 'Close form'
-                  : 'Add department'
-              }
-              image={AddIcon}
-              action={() => this.handleShowForm(FORMS.DEPARTMENT_FORM)}
-            />
-          </Section>
-        </Main>
-      </div>
-    );
-  }
+          )}
+          <Button
+            text={
+              showForm === FORMS.DEPARTMENT_FORM
+                ? 'Close form'
+                : 'Add department'
+            }
+            image={AddIcon}
+            action={() => handleShowForm(FORMS.DEPARTMENT_FORM)}
+          />
+        </Section>
+      </Main>
+    </div>
+  );
 }
+
+// import { Component } from 'react';
+// import {
+//   AddItemForm,
+//   Button,
+//   GeneralCardList,
+//   Main,
+//   Paper,
+//   Section,
+//   Sidebar,
+//   TutorForm,
+//   TutorsList,
+//   UniversityCard,
+// } from '../components';
+// import universityData from '../constants/universityData';
+// import TutorIcon from '../assets/images/teachers-emoji.png';
+// import CityIcon from '../assets/images/cities.svg';
+// import DepartmentIcon from '../assets/images/faculties-emoji.png';
+// import AddIcon from '../assets/images/add.svg';
+// import FORMS from 'constants/form';
+
+// export class App extends Component {
+//   state = {
+//     tutors: universityData?.tutors ?? [],
+//     cities:
+//       universityData?.cities.map(city => ({
+//         text: city,
+//         relation: 'cities',
+//       })) ?? [],
+//     departments:
+//       universityData?.department.map(({ name }) => ({
+//         text: name,
+//         relation: 'departments',
+//       })) ?? [],
+//     showForm: null,
+//   };
+//   addTutor = tutor => {
+//     this.setState(({ tutors }) => ({
+//       tutors: [...tutors, tutor],
+//       showForm: null,
+//     }));
+//   };
+
+//   deleteTutor = emailInput => {
+//     this.setState(({ tutors }) => {
+//       console.log([...tutors]);
+//       return {
+//         tutors: [...tutors].filter(({ email }) => email !== emailInput),
+//       };
+//     });
+//   };
+
+//   onEdit = () => {
+//     console.log('edit');
+//   };
+
+//   onDelete = () => {
+//     console.log('delete');
+//   };
+
+//   handleShowForm = formName => {
+//     this.setState(prevState => ({
+//       showForm: prevState.showForm === formName ? null : formName,
+//     }));
+//   };
+
+//   handleToogleMenu = () => {
+//     console.log('card');
+//   };
+
+//   addCity = name => {
+//     if (
+//       this.state.cities.some(
+//         city => city.text.toLowerCase() === name.toLowerCase()
+//       )
+//     ) {
+//       alert('City already exists');
+//     } else {
+//       const newCity = {
+//         text: name,
+//         relation: 'cities',
+//       };
+//       this.setState(prevState => ({
+//         cities: [...prevState.cities, newCity],
+//         showForm: null,
+//       }));
+//     }
+//   };
+
+//   addDepartment = name => {
+//     if (
+//       this.state.departments.some(
+//         department => department.text.toLowerCase() === name.toLowerCase()
+//       )
+//     ) {
+//       alert('Department already exists');
+//     } else {
+//       const newDepartment = {
+//         text: name,
+//         relation: 'departments',
+//       };
+//       this.setState(prevState => ({
+//         departments: [...prevState.departments, newDepartment],
+//         showForm: null,
+//       }));
+//     }
+//   };
+
+//   handleDeleteCard = (id, relation) => {
+//     this.setState(prevState => ({
+//       [relation]: prevState[relation].filter(({ text }) => text !== id),
+//     }));
+//   };
+
+//   handleEditCard = data => {
+//     const { id, relation, name } = data;
+//     const elemIndex = this.state[relation].findIndex(item => item.text === id);
+//     this.setState(prevState => ({
+//       [relation]: [
+//         ...prevState[relation].slice(0, elemIndex),
+//         { text: name, relation },
+//         ...prevState[relation].slice(elemIndex + 1),
+//       ],
+//     }));
+//   };
+
+//   render() {
+//     const { tutors, cities, departments, showForm } = this.state;
+//     console.log(cities);
+
+//     return (
+//       <div className="app">
+//         <Sidebar />
+
+//         <Main>
+//           <Section
+//             title="Information about university"
+//             isRightPosition
+//             isColumn
+//           >
+//             <UniversityCard
+//               name={universityData.name}
+//               onEdit={this.onEdit}
+//               onDelete={this.onDelete}
+//             ></UniversityCard>
+//             <Paper>
+//               <span>{universityData.description}</span>
+//             </Paper>
+//           </Section>
+
+//           <Section title="Tutors" image={TutorIcon}>
+//             <TutorsList tutors={tutors} deleteTutor={this.deleteTutor} />
+//             {showForm === FORMS.TUTOR_FORM && (
+//               <TutorForm addTutor={this.addTutor} />
+//             )}
+//             <Button
+//               text={showForm === FORMS.TUTOR_FORM ? 'Close form' : 'Add tutor'}
+//               image={AddIcon}
+//               action={() => this.handleShowForm(FORMS.TUTOR_FORM)}
+//             />
+//           </Section>
+
+//           <Section title="Cities" image={CityIcon}>
+//             <GeneralCardList
+//               listData={cities}
+//               deleteCard={this.handleDeleteCard}
+//               editCard={this.handleEditCard}
+//             />
+//             {showForm === FORMS.CITY_FORM && (
+//               <AddItemForm
+//                 title="add city"
+//                 placeholder="City"
+//                 onSubmit={this.addCity}
+//               />
+//             )}
+//             <Button
+//               text={showForm === FORMS.CITY_FORM ? 'Close form' : 'Add city'}
+//               image={AddIcon}
+//               action={() => this.handleShowForm(FORMS.CITY_FORM)}
+//             />
+//           </Section>
+
+//           <Section title="Departments" image={DepartmentIcon}>
+//             <GeneralCardList
+//               listData={departments}
+//               deleteCard={this.handleDeleteCard}
+//               editCard={this.handleEditCard}
+//             />
+//             {showForm === FORMS.DEPARTMENT_FORM && (
+//               <AddItemForm
+//                 title="add departments"
+//                 placeholder="Departments"
+//                 onSubmit={this.addDepartment}
+//               />
+//             )}
+//             <Button
+//               text={
+//                 showForm === FORMS.DEPARTMENT_FORM
+//                   ? 'Close form'
+//                   : 'Add department'
+//               }
+//               image={AddIcon}
+//               action={() => this.handleShowForm(FORMS.DEPARTMENT_FORM)}
+//             />
+//           </Section>
+//         </Main>
+//       </div>
+//     );
+//   }
+// }
--- src/components/GeneralCard/GeneralCardItem.jsx
+++ src/components/GeneralCard/GeneralCardItem.jsx
@@ -1,9 +1,11 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import { AddItemForm, Modal, Paper } from 'components';
 import { BsThreeDotsVertical } from 'react-icons/bs';
+
 import { ReactComponent as EditBtnIcon } from '../../assets/images/edit.svg';
 import { ReactComponent as DeleteBtnIcon } from '../../assets/images/delete.svg';
+
 import {
   StyledModalActionContainer,
   StyledBtn,
@@ -12,110 +14,224 @@ import {
   StyledItem,
 } from './GeneralCardItem.styled';
 
-export default class GeneralCardItem extends Component {
-  state = {
-    showDropdown: false,
-    showModal: null,
-    dropDownPosition: {
-      x: 0,
-      y: 0,
-    },
-  };
+export default function GeneralCardItem({
+  text,
+  relation,
+  editCard,
+  id,
+  deleteCard,
+}) {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [showModal, setShowModal] = useState(null);
+  const [dropDownPosition, setDropDownPosition] = useState({
+    x: 0,
+    y: 0,
+    clientWidth: 0,
+    clientHeight: 0,
+  });
 
-  handleShowDropDown = e => {
-    this.setState({
-      showDropdown: true,
-      dropDownPosition: {
-        x: e.clientX,
-        y: e.clientY,
-        clientWidth: document.documentElement.clientWidth,
-        clientHeight: document.documentElement.clientHeight,
-      },
+  const handleShowDropDown = e => {
+    setShowDropdown(true);
+    setDropDownPosition({
+      x: e.clientX,
+      y: e.clientY,
+      clientWidth: document.documentElement.clientWidth,
+      clientHeight: document.documentElement.clientHeight,
     });
   };
 
-  toggleDropDown = () => {
-    this.setState(({ showDropdown }) => ({
-      showDropdown: !showDropdown,
-    }));
+  const toggleDropDown = () => {
+    setShowDropdown(!showDropdown);
   };
 
-  handleCloseModal = () => {
-    this.setState({ showModal: null });
+  const handleCloseModal = () => {
+    setShowModal(null);
   };
 
-  handleDeleteBtnClick = () => {
-    this.handleToggleModal();
+  const handleActionBtnClick = action => {
+    setShowModal(action);
+    setShowDropdown(false);
   };
 
-  handleActionBtnClick = action => {
-    this.setState({ showModal: action, showDropdown: false });
-  };
+  return (
+    <Paper>
+      <StyledItem>
+        <span>{text}</span>
+        <StyledBtnMenu onClick={handleShowDropDown}>
+          <BsThreeDotsVertical />
+        </StyledBtnMenu>
 
-  render() {
-    const { text, id, relation, deleteCard, editCard } = this.props;
-    const { showDropdown, showModal, dropDownPosition } = this.state;
-
-    return (
-      <Paper>
-        <StyledItem>
-          <span>{text}</span>
-          <StyledBtnMenu onClick={this.handleShowDropDown}>
-            <BsThreeDotsVertical />
-          </StyledBtnMenu>
-
-          {showDropdown && (
-            <Modal onClose={this.toggleDropDown}>
-              <StyledContainer
-                x={dropDownPosition.x}
-                y={dropDownPosition.y}
-                clientWidth={dropDownPosition.clientWidth}
-                clientHeight={dropDownPosition.clientHeight}
+        {showDropdown && (
+          <Modal onClose={toggleDropDown}>
+            <StyledContainer
+              x={dropDownPosition.x}
+              y={dropDownPosition.y}
+              clientWidth={dropDownPosition.clientWidth}
+              clientHeight={dropDownPosition.clientHeight}
+            >
+              <StyledBtn
+                type="button"
+                onClick={() => handleActionBtnClick('edit')}
               >
-                <StyledBtn
-                  type="button"
-                  onClick={() => this.handleActionBtnClick('edit')}
-                >
-                  <EditBtnIcon />
-                  Edit
-                </StyledBtn>
-                <StyledBtn
-                  type="button"
-                  onClick={() => this.handleActionBtnClick('delete')}
-                >
-                  <DeleteBtnIcon />
-                  Delete
-                </StyledBtn>
-              </StyledContainer>
-            </Modal>
-          )}
-        </StyledItem>
-
-        {showModal === 'edit' && (
-          <Modal onClose={this.handleCloseModal}>
-            <StyledModalActionContainer>
-              <h2>Edit {relation === 'departments' ? 'department' : 'city'}</h2>
-              <AddItemForm
-                onSubmit={editCard}
-                idItem={id}
-                relation={relation}
-              ></AddItemForm>
-            </StyledModalActionContainer>
+                <EditBtnIcon />
+                Edit
+              </StyledBtn>
+              <StyledBtn
+                type="button"
+                onClick={() => handleActionBtnClick('delete')}
+              >
+                <DeleteBtnIcon />
+                Delete
+              </StyledBtn>
+            </StyledContainer>
           </Modal>
         )}
+      </StyledItem>
 
-        {showModal === 'delete' && (
-          <Modal onClose={this.handleCloseModal}>
-            <StyledModalActionContainer>
-              <h2>
-                Delete {relation === 'departments' ? 'department' : 'city'}
-              </h2>
-              <button onClick={() => deleteCard(id, relation)}>Yes</button>
-              <button onClick={() => this.handleCloseModal()}>No</button>
-            </StyledModalActionContainer>
-          </Modal>
-        )}
-      </Paper>
-    );
-  }
+      {showModal === 'edit' && (
+        <Modal onClose={handleCloseModal}>
+          <StyledModalActionContainer>
+            <h2>Edit {relation === 'departments' ? 'department' : 'city'}</h2>
+            <AddItemForm
+              onSubmit={editCard}
+              idItem={id}
+              relation={relation}
+            ></AddItemForm>
+          </StyledModalActionContainer>
+        </Modal>
+      )}
+
+      {showModal === 'delete' && (
+        <Modal onClose={handleCloseModal}>
+          <StyledModalActionContainer>
+            <h2>Delete {relation === 'departments' ? 'department' : 'city'}</h2>
+            <button onClick={() => deleteCard(id, relation)}>Yes</button>
+            <button onClick={() => handleCloseModal()}>No</button>
+          </StyledModalActionContainer>
+        </Modal>
+      )}
+    </Paper>
+  );
 }
+
+// import { Component } from 'react';
+
+// import { AddItemForm, Modal, Paper } from 'components';
+// import { BsThreeDotsVertical } from 'react-icons/bs';
+// import { ReactComponent as EditBtnIcon } from '../../assets/images/edit.svg';
+// import { ReactComponent as DeleteBtnIcon } from '../../assets/images/delete.svg';
+// import {
+//   StyledModalActionContainer,
+//   StyledBtn,
+//   StyledBtnMenu,
+//   StyledContainer,
+//   StyledItem,
+// } from './GeneralCardItem.styled';
+
+// export default class GeneralCardItem extends Component {
+//   state = {
+//     showDropdown: false,
+//     showModal: null,
+//     dropDownPosition: {
+//       x: 0,
+//       y: 0,
+//     },
+//   };
+
+//   handleShowDropDown = e => {
+//     this.setState({
+//       showDropdown: true,
+//       dropDownPosition: {
+//         x: e.clientX,
+//         y: e.clientY,
+//         clientWidth: document.documentElement.clientWidth,
+//         clientHeight: document.documentElement.clientHeight,
+//       },
+//     });
+//   };
+
+//   toggleDropDown = () => {
+//     this.setState(({ showDropdown }) => ({
+//       showDropdown: !showDropdown,
+//     }));
+//   };
+
+//   handleCloseModal = () => {
+//     this.setState({ showModal: null });
+//   };
+
+//   handleDeleteBtnClick = () => {
+//     this.handleToggleModal();
+//   };
+
+//   handleActionBtnClick = action => {
+//     this.setState({ showModal: action, showDropdown: false });
+//   };
+
+//   render() {
+//     const { text, id, relation, deleteCard, editCard } = this.props;
+//     const { showDropdown, showModal, dropDownPosition } = this.state;
+
+//     return (
+//       <Paper>
+//         <StyledItem>
+//           <span>{text}</span>
+//           <StyledBtnMenu onClick={this.handleShowDropDown}>
+//             <BsThreeDotsVertical />
+//           </StyledBtnMenu>
+
+//           {showDropdown && (
+//             <Modal onClose={this.toggleDropDown}>
+//               <StyledContainer
+//                 x={dropDownPosition.x}
+//                 y={dropDownPosition.y}
+//                 clientWidth={dropDownPosition.clientWidth}
+//                 clientHeight={dropDownPosition.clientHeight}
+//               >
+//                 <StyledBtn
+//                   type="button"
+//                   onClick={() => this.handleActionBtnClick('edit')}
+//                 >
+//                   <EditBtnIcon />
+//                   Edit
+//                 </StyledBtn>
+//                 <StyledBtn
+//                   type="button"
+//                   onClick={() => this.handleActionBtnClick('delete')}
+//                 >
+//                   <DeleteBtnIcon />
+//                   Delete
+//                 </StyledBtn>
+//               </StyledContainer>
+//             </Modal>
+//           )}
+//         </StyledItem>
+
+//         {showModal === 'edit' && (
+//           <Modal onClose={this.handleCloseModal}>
+//             <StyledModalActionContainer>
+//               <h2>Edit {relation === 'departments' ? 'department' : 'city'}</h2>
+//               <AddItemForm
+//                 onSubmit={editCard}
+//                 idItem={id}
+//                 relation={relation}
+//               ></AddItemForm>
+//             </StyledModalActionContainer>
+//           </Modal>
+//         )}
+
+//         {showModal === 'delete' && (
+//           <Modal onClose={this.handleCloseModal}>
+//             <StyledModalActionContainer>
+//               <h2>
+//                 Delete {relation === 'departments' ? 'department' : 'city'}
+//               </h2>
+//               <button onClick={() => deleteCard(id, relation)}>Yes</button>
+//               <button onClick={() => this.handleCloseModal()}>No</button>
+//             </StyledModalActionContainer>
+//           </Modal>
+//         )}
+//       </Paper>
+//     );
+//   }
+// }
